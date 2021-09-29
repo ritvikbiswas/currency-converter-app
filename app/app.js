ba44c808dda8d095c4f2adea4cf2b994b9b4b824
Your Name <you@example.com>
2021-09-29 17:12:27 -0400

    done!

--- app/app.js
+++ app/app.js
@@ -8,21 +8,32 @@ myApp.controller('AppController', [
 ]);
 
 myApp.controller('TickerController', [
-    '$scope', '$timeout', '$interval', 
-    function($scope, $timeout, $interval){
-        $scope.boxes = [
-            {title: 'Box 1'},
-            {title: 'Box 2'},
-            {title: 'Box 3'},
-            {title: 'Box 4'},
-            {title: 'Box 5'},
-            {title: 'Box 6'},
-            {title: 'Box 7'},
-            {title: 'Box 8'},
-            {title: 'Box 9'},
-            {title: 'Box 10'}
-        ]; 
+    '$scope', '$timeout', '$interval', '$http', 
+    function($scope, $timeout, $interval, $http){
+        let rates = {};
+        $http.get("https://api.frankfurter.app/latest?from=USD").then(
+            function success(response){
+                rates = response.data.rates;
+                $scope.boxes = [
+                    {title: 'USD → GBP', value: "1 → " + rates.GBP},
+                    {title: 'USD → EUR', value: "1 → " + rates.EUR},
+                    {title: 'USD → CAD', value: "1 → " + rates.CAD},
+                    {title: 'USD → CNY', value: "1 → " + rates.CNY},
+                    {title: 'USD → INR', value: "1 → " + rates.INR},
+                    {title: 'USD → HKD', value: "1 → " + rates.HKD},
+                    {title: 'USD → JPY', value: "1 → " + rates.JPY},
+                    {title: 'USD → SGD', value: "1 → " + rates.SGD},
+                    {title: 'USD → RUB', value: "1 → " + rates.RUB},
+                    {title: 'USD → AUD', value: "1 → " + rates.AUD},
+                ]; 
+            },
+            function failure(response){
+                alert("Forex API Server is not responding. Please try again later.");
+                console.log("Unable to perform get request.");
+            }
+        );
 
+    
         $scope.moving = false;
         $scope.moveLeft = function(){
             $scope.moving = true;
@@ -39,10 +50,56 @@ myApp.controller('TickerController', [
 ]);
 
 myApp.controller('MenuController', [
-    '$scope',
-    function($scope){
+    '$scope', '$http',
+    function($scope, $http){
+        /**
+         * Helper Function - formats a JS input date into a yyyy-mm-dd
+         * https://stackoverflow.com/a/23593099
+         */
+        function formatDate(date) {
+            var d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+        
+            if (month.length < 2) 
+                month = '0' + month;
+            if (day.length < 2) 
+                day = '0' + day;
+        
+            return [year, month, day].join('-');
+        }
+
         $scope.convert = function(userInput){
-            console.log(userInput);
+            let currencies = ["USD", "GBP", "EUR", "CAD", "CNY", "INR", "HKD", "JPY", "SGD"];
+            //console.log(formatDate(userInput.date));
+
+            let currencyInput = userInput.currency;
+            let valueInput = userInput.value;
+            let dateInput = formatDate(userInput.date);
+
+            currencies = currencies.filter(item => item != currencyInput);
+            //console.log(currencies);
+
+            let url = "https://api.frankfurter.app/"+dateInput+"?amount="+valueInput+"&from="+currencyInput;
+            console.log(url);
+
+            $http.get(url).then(
+                function success(response){
+                    rates = response.data.rates; //object of rates
+
+                    //convert to array so we can filter out the currencies we don't need
+                    rates = Object.entries(rates); //convert to array
+                    rates = rates.filter(([key,value]) => currencies.includes(key)); //filter
+                    rates = Object.fromEntries(rates);
+                    console.log(rates);
+                    $scope.conversions = rates;
+                },
+                function failure(response){
+                    alert("Forex API Server is not responding. Please try again later.");
+                    console.log("Unable to perform get request.");
+                }
+            );
         }
     }
 ]);
