7a0fd7b5f2f22650e9dce6b27344bb7d2d473ae4
Your Name <you@example.com>
2021-10-04 15:53:47 -0400

    changes on this end as to add average func in app

--- app/app.js
+++ app/app.js
@@ -103,3 +103,58 @@ myApp.controller('MenuController', [
         }
     }
 ]);
+
+myApp.controller('MenuController', [
+    '$scope', '$http',
+    function($scope, $http){
+        /**
+         * Helper Function - formats a JS input date into a yyyy-mm-dd
+         * https://stackoverflow.com/a/23593099
+         */
+        function formatDate(date) {
+            var d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+        
+            if (month.length < 2) 
+                month = '0' + month;
+            if (day.length < 2) 
+                day = '0' + day;
+        
+            return [year, month, day].join('-');
+        }
+
+        $scope.findRate = function(userInput){
+            let currencies = ["USD", "GBP", "EUR", "CAD", "CNY", "INR", "HKD", "JPY", "SGD"];
+            //console.log(formatDate(userInput.date));
+
+            let currencyInput = userInput.currency;
+            let valueInput = userInput.value;
+            let dateInput = formatDate(userInput.date);
+
+            currencies = currencies.filter(item => item != currencyInput);
+            //console.log(currencies);
+
+            let url = "https://api.frankfurter.app/"+dateInput+"?amount="+valueInput+"&from="+currencyInput;
+            console.log(url);
+
+            $http.get(url).then(
+                function success(response){
+                    rates = response.data.rates; //object of rates
+
+                    //convert to array so we can filter out the currencies we don't need
+                    rates = Object.entries(rates); //convert to array
+                    rates = rates.filter(([key,value]) => currencies.includes(key)); //filter
+                    rates = Object.fromEntries(rates);
+                    console.log(rates);
+                    $scope.conversions = rates;
+                },
+                function failure(response){
+                    alert("Forex API Server is not responding. Please try again later.");
+                    console.log("Unable to perform get request.");
+                }
+            );
+        }
+    }
+]);
